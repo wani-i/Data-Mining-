fe0ee39e2aafab533bfb5f5340c97c0e99b82c42
wani-i <dwanderturf@gmail.com>
2025-09-15 23:08:22 +0800

    First commit

---
+++ assign3_DM.R
@@ -0,0 +1,9 @@
+  temp <- 25
+  
+  if(temp >= 30){
+    print("Hot")
+  }else if(temp >= 20 & temp <= 29){
+    print("Warm")
+  }else if(temp < 20){
+    print("Cold")
+  }
---
+++ assign4_DM.R
@@ -0,0 +1,11 @@
+  age <- 65
+  
+  if(age < 13){
+    print("Child")
+  } else if (age >= 13 & age <= 19){
+    print("Teenager")
+  }else if(age >= 20 & age <= 59){
+    print("Adult")
+  }else if(age > 60){
+    print("Senior")
+  }
